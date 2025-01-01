202aa769d71c5f34fa5e13d546857e052476e91e
conycomy <imgyeong4936@gmail.com>
2025-01-01 20:40:35 +0900

    modified blogApicontroller, BlogService and add file dto , resources, test code

--- src/main/java/me/imgyeong/springbootdeveloper/controller/BlogApiController.java
+++ src/main/java/me/imgyeong/springbootdeveloper/controller/BlogApiController.java
@@ -1,4 +1,43 @@
 package me.imgyeong.springbootdeveloper.controller;
 
+import lombok.RequiredArgsConstructor;
+import me.imgyeong.springbootdeveloper.domain.Article;
+import me.imgyeong.springbootdeveloper.dto.AddArticleRequest;
+import me.imgyeong.springbootdeveloper.dto.ArticleResponse;
+import me.imgyeong.springbootdeveloper.service.BlogService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
 public class BlogApiController {
+	private final BlogService blogService;
+
+	// HTTP 메서드가 POST 일때 전달받은 URL과 동일 하면 메서드로 매핑
+	@PostMapping("/api/articles")
+	//@RequestBody로 요청 본문 값 매핑
+	public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest request) {
+		Article savedArticle = blogService.save(request);
+		// 요청한 자원이 성공적 으로 생성 되었으며 저장된 블로그 글 정보를 응답 객체에 전송
+		return ResponseEntity.status(HttpStatus.CREATED)
+			.body(savedArticle);
+	}
+
+	@GetMapping("/api/articles")
+	public ResponseEntity<List<ArticleResponse>> findAllArticles() {
+		List<ArticleResponse> articles = blogService.findAll()
+			.stream()
+			.map(ArticleResponse::new)
+			.toList();
+
+		return ResponseEntity.ok()
+			.body(articles);
+	}
 }
+
--- src/main/java/me/imgyeong/springbootdeveloper/service/BlogService.java
+++ src/main/java/me/imgyeong/springbootdeveloper/service/BlogService.java
@@ -7,13 +7,18 @@ import me.imgyeong.springbootdeveloper.dto.AddArticleRequest;
 import me.imgyeong.springbootdeveloper.repository.BlogRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @Service
 public class BlogService {
 	private final BlogRepository blogRepository;
 
 		public Article save(AddArticleRequest request){
-		return blogRepository.save(request.toEntity());
+			return blogRepository.save(request.toEntity());
 		}
 
+		public List<Article> findAll() {
+			return blogRepository.findAll();
+		}
 }
